5936e17a71da77427fe4f14663e9e89544aca2a1
Félix Hernández Hernández <felix_hdezz95@hotmail.com>
2018-07-19 18:27:11 -0500

    Add files via upload

---
+++ Apuntes.js
@@ -0,0 +1,20 @@
+//Puntos resueltos
+
+/*
+1.- Quitarle identado a los semaforos, dejar el identado en los textos
+2.- Al hacer un filtro en el N1, lo lleva correctamente al N2, pero al regresar a N1, el filtro lo deja en el combo del filtro con el codigo y no con la descripción
+3.- La division de miles desde de ser coma(formato MX) no punto(formato Arg)
+4.- Primera columna de vista detalle esta demasiado ancha(ya esta resulto por ternium)
+5.- Configuracion de las columnas de indicador interno
+6.- desfase de los rows en el n3 de detalle
+7.- Primera columna(gris) en vista detalle no esta alineada con el resto
+8.- Seccion camiones esta abierto, debe estar igual que volumen cerrado
+9.- Las columnas de Real, Objetivo y desvio tienen tañamos diferentes 
+*/
+
+
+// Pendientes
+/*
+1.- desfase de los rows en el n3 de detalle
+6.- Primera columna(gris) en vista detalle no esta alineada con el resto
+*/
